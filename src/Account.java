32a2411b57714944a1e4f5c7906f5ecbb2c094c9
Noxtrah <dokayra02@gmail.com>
2022-06-04 10:17:37 +0300

    Encapsulated Account Class

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,29 @@
 public class Account {
+    private double balance;
+    private double interest;
+    private int accountNumber;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Menu {
@@ -7,7 +8,8 @@ public class Menu {
     boolean exit;
 
     public static void main(String[] args){
-
+        Menu menu = new Menu();
+        menu.runMenu();
     }
 
     public void runMenu(){
@@ -26,15 +28,80 @@ public class Menu {
                 System.exit(0);
                 break;
             case 1:
-                //createAnAccount();
+                createAnAccount();
             case 2:
-                //makeADeposit();
+                makeADeposit();
             case 3:
-                //makeAWithdrawal();
+                makeAWithdrawal();
             case 4:
-                //listBalances();
+                listBalances();
             default:
                 System.out.println("Unknown error has occurred.");
+        }
+    }
+
+    private void listBalances() {
+    }
+
+    private void makeAWithdrawal() {
+    }
+
+    private void makeADeposit() {
+    }
+
+    private void createAnAccount() {
+        String firstName, lastName, ssn, accountType = "";
+        double initialDeposit = 0;
+        boolean valid = false;
+        while(!valid){
+            System.out.println("Please enter an account type (checking/savings)");
+            accountType = keyboard.nextLine();
+            if (accountType.equalsIgnoreCase("checking") || accountType.equalsIgnoreCase("savings")){
+                valid = true;
+            }
+            else{
+                System.out.println("Invalid account type selected. Please enter checking or savings.");
+            }
+        }
+        System.out.println("Please enter your first name: ");
+        firstName = keyboard.nextLine();
+        System.out.println("Please enter your last name: ");
+        lastName = keyboard.nextLine();
+        System.out.println("Please enter your social security number: ");
+        ssn = keyboard.nextLine();
+        valid = false;
+        while (!valid){
+            System.out.println("Please enter initial deposit: ");
+            try{
+                initialDeposit = Double.parseDouble(keyboard.nextLine());
+            }
+            catch(NumberFormatException e){
+                System.out.println("Deposit must be a number");
+            }
+            if (accountType.equalsIgnoreCase("checking")){
+                if(initialDeposit < 100){
+                    System.out.println("Checking accounts require minimum $100 to open.");
+                }
+                else{
+                    valid = true;
+                }
+            }
+            else if (accountType.equalsIgnoreCase("savings")){
+                if (initialDeposit < 50){
+                    System.out.println("Savings accounts require minimum $50 to open.");
+                }
+                else {
+                    valid = true;
+                }
+            }
+        }
+        //We can create an account now
+        Account account = new Account();
+        if (accountType.equalsIgnoreCase("cheking")){
+            account = new Checking(initialDeposit);
+        }
+        else {
+            account = new Savings(initialDeposit);
         }
     }
 
