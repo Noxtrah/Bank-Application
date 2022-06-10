18dded4520edb676bf3d22079327746be89f45cc
Noxtrah <dokayra02@gmail.com>
2022-06-10 15:30:09 +0300

    No DatabaseService

--- src/Checking.java
+++ src/Checking.java
@@ -2,13 +2,12 @@ public class Checking extends Account{
     private static String accountType = "Cheking";
 
     Checking(double initialDeposit){
-        super();
         this.setBalance(initialDeposit);
         this.checkInterest(0);
     }
 
     public String toString(){
-        return "Account Type: " + accountType + "Account\n" +
+        return "Account Type: " + accountType + " Account\n" +
                 "Account Number: " + this.getAccountNumber() + "\n" +
                 "Balance: " + this.getBalance() + "\n" +
                 "Interest Rate: " + this.getInterest() + "%\n";
---
+++ src/InvalidAccountTypeException.java
@@ -0,0 +1,5 @@
+class InvalidAccountTypeException extends Exception {
+    public InvalidAccountTypeException(){
+        super("Invalid Account Type Selected.");
+    }
+}
